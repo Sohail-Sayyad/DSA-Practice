e4985f55dbe5e9553bf43e26106a17f0e41680d3
Sohail Sayyad <sohailmsayyad@gmail.com>
2026-07-13 22:21:09 +0530

    Learned about hashing using maps

--- hashing.cpp
+++ hashing.cpp
@@ -1,6 +1,7 @@
 //Number of times the element appearing
 
 #include<iostream>
+#include<map>
 using namespace std;
 int main(){
 
@@ -76,7 +77,7 @@ while(q--){
 return 0;
 }
 
-*/
+
 int n;
 string s;
 cout<<"Enter the value of string: ";
@@ -99,6 +100,44 @@ while(q--){
     cin>>c;
 
 cout<<hash[c-'a']<<endl;
+}
+
+return 0;
+}
+
+*/
+
+int n;
+cout<<"Enter the value of n: ";
+cin>>n;
+
+int arr[n];
+for(int i=0; i<n; i++){
+    cin>>arr[i];
+}
+
+
+//precompute
+
+map<int, int> mpp;
+for(int i=0; i<n; i++){
+    mpp[arr[i]]++;
+}
+
+for(auto it:mpp){
+    cout<< it.first << "->" << it.second << endl;
+}
+
+
+int q;
+cout<<"Enter the number of queries: ";
+cin>>q;
+while(q--){
+    int number;
+    cout<<"Enter the number: ";
+    cin>>number;
+
+    cout<< mpp[number] << endl;
 }
 
 return 0;
